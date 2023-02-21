edf03d544c74e876da8b4c314d6bdf2ea49cd888
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2023-02-21 12:50:36 +0300

    add neighbors functions

--- lib/include/utils.hpp
+++ lib/include/utils.hpp
@@ -9,6 +9,7 @@
 
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <span>
 #include <vector>
@@ -16,12 +17,12 @@
 namespace red::containers::utils
 {
 
-template <typename T> std::vector<int> get_left_neighbors (std::span<const T> sequence)
+template <typename T, typename F> std::vector<int> get_neighbors (std::span<T> sequence, F loop)
 {
     std::vector<unsigned> stack {};
     std::vector<int> neighbors {};
-    for ( unsigned i = 0; i < sequence.size (); ++i )
-    {
+
+    auto iteration = [&stack, &neighbors, &sequence] (auto i) {
         while ( (!stack.empty ()) && (sequence[i] <= sequence[stack.back ()]) )
             stack.pop_back ();
 
@@ -30,8 +31,36 @@ template <typename T> std::vector<int> get_left_neighbors (std::span<const T> se
         else
             neighbors.push_back (stack.back ());
         stack.push_back (i);
-    }
+    };
+
+    loop (iteration);
     return neighbors;
+}
+
+template <typename T> std::vector<int> get_left_neighbors (std::span<T> sequence)
+{
+    auto size = sequence.size ();
+
+    auto straight_loop = [size] (auto iteration) {
+        for ( unsigned i = 0; i < size; ++i )
+            iteration (i);
+    };
+
+    return get_neighbors (sequence, straight_loop);
+}
+
+template <typename T> std::vector<int> get_right_neighbors (std::span<T> sequence)
+{
+    auto size = sequence.size ();
+
+    auto reverse_loop = [size] (auto iteration) {
+        for ( int i = size - 1; i >= 0; --i )
+            iteration (i);
+    };
+
+    auto right_neighbors = get_neighbors (sequence, reverse_loop);
+    std::reverse (right_neighbors.begin (), right_neighbors.end ());
+    return right_neighbors;
 }
 
 }   // namespace red::containers::utils
--- lib/test/src/utils.cc
+++ lib/test/src/utils.cc
@@ -5,10 +5,22 @@
 #include <iostream>
 #include <vector>
 
-TEST (test_utils, test_lefT_neighbors)
+TEST (test_utils, test_left_neighbors)
 {
     std::vector a {4, 7, 8, 1, 2, 3, 9, 5, 6};
-    std::vector b = red::containers::utils::get_left_neighbors<int> (std::span {a});
+
+    std::vector left_neighbors = red::containers::utils::get_left_neighbors (std::span {a});
+
     std::vector res {-1, 0, 1, -1, 3, 4, 5, 5, 7};
-    EXPECT_EQ (res, b);
+    EXPECT_EQ (res, left_neighbors);
+}
+
+TEST (test_utils, test_right_neighbors)
+{
+    std::vector a {4, 7, 8, 1, 2, 3, 9, 5, 6};
+
+    std::vector right_neighbors = red::containers::utils::get_right_neighbors (std::span {a});
+
+    std::vector res {3, 3, 3, -1, -1, -1, 7, -1, -1};
+    EXPECT_EQ (res, right_neighbors);
 }
